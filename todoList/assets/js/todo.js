66b2312979c57c6f5d24a888256fd0c5474d2ec6
kaymohh <thrilltim@gmail.com>
2023-06-13 14:36:45 +0100

    logic done. unto the css

---
+++ todoList/assets/js/todo.js
@@ -0,0 +1,27 @@
+// const { event } = require("jquery");
+
+$("ul").on("click", "li", function () {
+  $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function (e) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
+
+  e.stopPropagation();
+});
+
+$("input[type='text'").keypress(function (event) {
+  if (event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
+});
+
+// $("#whichkey").on("keydown", function (event) {
+//   $("#log").html(event.type + ": " + event.which);
+// });
