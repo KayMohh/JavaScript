c2766ee9d193321f5f278da58210c28e3ca3c334
kaymohh <thrilltim@gmail.com>
2023-06-19 18:55:07 +0100

    Portfolio update

--- kmPortfolioProj/script.js
+++ kmPortfolioProj/script.js
@@ -1,9 +1,24 @@
-var skills = document.querySelector("#skills");
-var exp = document.querySelector("#experience");
-var edu = document.querySelector("#education");
+var tablinks = document.getElementsByClassName("tab-links");
 
-skills.addEventListener("click", function () {
-  skills.classList.add("selected");
-  exp.classList.remove("selected");
-  alert("Hello");
-});
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function expandtab() {
+  for (tablink of tablinks) {
+    tablink.classList.remove("is-active");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("is-active");
+  }
+  event.currentTarget.classList.add("is-active");
+  document.getElementById(tabname).classList.add("is-active");
+}
+
+// var skills = document.querySelector("#skills");
+// var exp = document.querySelector("#experience");
+// var edu = document.querySelector("#education");
+
+// skills.addEventListener("click", function () {
+//   skills.classList.add("selected");
+//   exp.classList.remove("selected");
+//   alert("Hello");
+// });
